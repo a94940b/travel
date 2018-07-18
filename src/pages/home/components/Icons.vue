fccd65df471add0a007984b526e1ff496601ac30
bohan <784368611@qq.com>
2018-07-18 11:37:02 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) of pages" v-bind:key="index">
+        <div
+          class="icon"
+          v-for="item of page"
+          :key="item.id"
+        >
+          <div class="icon-image">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      iconList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '广州必游'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: '游乐场'
+      }, {
+        id: '005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/89/55083b0f1951f302.png',
+        desc: '游船游艇'
+      }, {
+        id: '006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png',
+        desc: '夏日玩水'
+      }, {
+        id: '007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '城市观光'
+      }, {
+        id: '008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+        desc: '玩转长隆'
+      }, {
+        id: '010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: '全部玩乐'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-image
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
+</style>
