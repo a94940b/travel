2ccab5b57b4dfe2414037cf6eeba1ecb5aebbb1b
bohan <784368611@qq.com>
2018-07-19 23:51:56 +0800

    finish pages keepalive

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- <keep-alive>标签是vue自带的，把router-view包裹在内，表示通过路由第一次加载过页面之后，将其放入内存之中，下次再打开的时候就不会再重新通过Ajax发送http请求，而是直接从内存中去读取页面，提高了页面的性能 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
