ba94c91275e03409c2bfaa153026e56fdea40ff0
bohan <784368611@qq.com>
2018-08-13 22:20:46 +0800

    路由设置成history模式

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // 在 build/webpack.base.conf.js 文件 第39行自定义了一个alias(别名), 'styles': resolve('src/assets/styles') ，然后就可以用styles来指代styles的文件夹地址，简化了import引入文件时的代码
+// fastClick插件，解决某些机型和浏览器click延迟300ms触发的问题。
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -13,6 +14,7 @@ import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+// 使用fastclick解决移动端点击事件300ms延迟
 fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
 
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ import Detail from '@/pages/detail/Detail'
 Vue.use(Router)
 
 export default new Router({
+  // 路由默认的是哈希模式，会用#表示根页面
+  // 调整成history模式，和传统服务端请求的url地址一致，首页url没有#符号，更直观。
+  // '/detail/:id'是动态路由，通过传入不同的id获取不同页面，比如请求编号101的商品的详情页面 /detail/101
+  // 不传入参数只有/detail则请求不到页面。
+  mode: 'history',
   routes: [{
     path: '/',
     name: 'Home',
