4586bce0309d1ee5fe1ff1f19d38533affbb19db
bohan <784368611@qq.com>
2018-08-13 18:53:08 +0800

    重新调整回用MOCK拿取数据

--- config/index.js
+++ config/index.js
@@ -12,7 +12,10 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         '/api': {
-            target: 'http://localhost:80'
+            target: 'http://localhost:8080',
+            pathRewrite: {
+                '^/api': '/static/mock'
+            }
         }
     },
 
@@ -54,7 +57,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/qunar',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
